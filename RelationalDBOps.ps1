0335d9ba8457768d54e68897815cf8861b5b6947
Cansın Aldanmaz <canald@gmail.com>
2017-01-30 16:41:07 +0300

    Update RelationalDBOps.ps1

--- RelationalDBOps.ps1
+++ RelationalDBOps.ps1
@@ -32,6 +32,24 @@ function select-Oracle
     }
 }
 
+function Select-MSSQL {
+    param(
+        [string] $connectionString = $(throw "Please specify a connection string."),
+        [string] $sqlCommand = $(throw "Please specify a query.")
+      )
+
+    $connection = new-object system.data.SqlClient.SQLConnection($connectionString)
+    $command = new-object system.data.sqlclient.sqlcommand($sqlCommand,$connection)
+    $connection.Open()
+
+    $adapter = New-Object System.Data.sqlclient.sqlDataAdapter $command
+    $dataset = New-Object System.Data.DataSet
+    $adapter.Fill($dataSet) | Out-Null
+
+    $connection.Close()
+    $dataSet.Tables
+
+}
 function select-MySql
 {
     [CmdletBinding()]
