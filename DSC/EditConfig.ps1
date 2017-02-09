c653eeb6e79ee172c9b0e3223732ac8b55b79a6d
Cansın Aldanmaz <canald@gmail.com>
2017-02-09 17:12:00 +0300

    Update EditConfig.ps1

--- DSC/EditConfig.ps1
+++ DSC/EditConfig.ps1
@@ -1,89 +1,90 @@
 Configuration changeConfig
 {
-    Param(
-    [string]$path, 
-    [string]$NodePath, 
-    [string]$NamespaceURI = "", 
-    [string]$NodeSeparatorCharacter = '>',
-    [string]$attributeName,
-    [string]$newValue
-    )
-    
+   
     Import-DscResource –ModuleName 'PSDesiredStateConfiguration'
     Node "localhost"
     {
-        File exists
+        foreach($key in ($OctopusParameters.keys | ? {$_ -like "DSC__Config__NodePath*"} )) 
         {
-            Ensure = "Present"
-            DestinationPath = $path
-            Type = 'File'
+            $attributeName = $OctopusParameters[$key].Split('>')[-1]
+            write-verbose -message "setting attributeName:$attributeName" -verbose
+            $id = $key.Split('__')[-1]
+            write-verbose -message "setting id:$id" -verbose
+            $configPath = $OctopusParameters["DSC__Config__FilePath__"+$id]
+            write-verbose -message "setting configPath:$configPath" -verbose
+            $newValue = $OctopusParameters["DSC__Config__Value__"+$id]
+            write-verbose -message "setting newValue:$newValue " -verbose
+            $nodePath = $OctopusParameters["DSC__Config__NodePath__"+$id].Replace(">$attributeName","")
+            write-verbose -message "setting nodePath:$nodePath" -verbose
+            $NamespaceURI = $OctopusParameters["DSC__Config__NamespaceURITemp__"+$id]
+            
+            write-verbose -message "setting NamespaceURITemp:$NamespaceURITemp" -verbose
+            File "FileCheck-$id"
+            {
+                Ensure = "Present"
+                DestinationPath = $configPath
+                Type = 'File'
+            }
+            
+            Script "ChangeConfig-$id" 
+            {
+                GetScript = 
+                {
+                    write-verbose -Message "Creating XMLDocument using Path:$($using:configPath)" -verbose
+                    $XmlDocument = [xml]$(get-content -Path $($using:configPath) -ErrorAction Stop)
+                    write-verbose -Message "Created XMLDocument:$XmlDocument" -verbose
+                    $NamespaceURITemp = $using:NamespaceURI
+                    if([string]::IsNullOrEmpty($NamespaceURITemp)) { $NamespaceURITemp = $XmlDocument.DocumentElement.NamespaceURI }
+                    $xmlNsManager = New-Object System.Xml.XmlNamespaceManager($XmlDocument.NameTable)
+                    $xmlNsManager.AddNamespace("ns", $NamespaceURITemp)
+                    $tempPath = $using:nodePath
+                    $fullyQualifiedNodePath = "/ns:$($tempPath.Replace('>', '/ns:'))"
+                    $node = $XmlDocument.SelectSingleNode($fullyQualifiedNodePath, $xmlNsManager)
+                    return @{Result = "$($node.GetAttribute($using:attributeName))"}
+                }
+                SetScript =
+                {    
+                write-verbose -Message "Creating XMLDocument using Path:$($using:configPath)" -verbose
+                $XmlDocument = [xml]$(get-content -Path $($using:configPath) -ErrorAction Stop)
+                write-verbose -Message "Created XMLDocument:$XmlDocument" -verbose
+                $NamespaceURITemp = $using:NamespaceURI
+                if([string]::IsNullOrEmpty($NamespaceURITemp)) { $NamespaceURITemp = $XmlDocument.DocumentElement.NamespaceURI }
+                $xmlNsManager = New-Object System.Xml.XmlNamespaceManager($XmlDocument.NameTable)
+                $xmlNsManager.AddNamespace("ns", $NamespaceURITemp)
+                $tempPath = $using:nodePath
+                $fullyQualifiedNodePath = "/ns:$($tempPath.Replace('>', '/ns:'))"
+                $node = $XmlDocument.SelectSingleNode($fullyQualifiedNodePath, $xmlNsManager)
+                $node.SetAttribute($using:attributeName,$using:newValue)
+                $XmlDocument.Save($($using:configPath))
         }
-
-        Script configDeneme 
-        {
-       GetScript = 
-       {
-           $XmlDocument = [xml]$(get-content -Path $using:path -ErrorAction Stop)
-           $NamespaceURITemp = $using:NamespaceURI
-           if([string]::IsNullOrEmpty($NamespaceURITemp)) { $NamespaceURITemp = $XmlDocument.DocumentElement.NamespaceURI } 
-           $xmlNsManager = New-Object System.Xml.XmlNamespaceManager($XmlDocument.NameTable)
-           $xmlNsManager.AddNamespace("ns", $NamespaceURITemp)
-           $tempPath = $using:NodePath
-           $fullyQualifiedNodePath = "/ns:$($tempPath.Replace($($using:NodeSeparatorCharacter), '/ns:'))"
-           $node = $XmlDocument.SelectSingleNode($fullyQualifiedNodePath, $xmlNsManager)
-           
-           
-           return @{
-               Result = "$($node.GetAttribute($using:attributeName))"
-           }
-       }
-       SetScript =
-       {    
-           $XmlDocument = [xml]$(get-content -Path $using:path -ErrorAction Stop)
-           $NamespaceURITemp = $using:NamespaceURI
-           if([string]::IsNullOrEmpty($NamespaceURITemp)) { $NamespaceURITemp = $XmlDocument.DocumentElement.NamespaceURI } 
-           $xmlNsManager = New-Object System.Xml.XmlNamespaceManager($XmlDocument.NameTable)
-           $xmlNsManager.AddNamespace("ns", $NamespaceURITemp)
-          
-           $tempPath = $using:NodePath
-           Write-Verbose -Message "tempPath:$tempPath" -Verbose
-           $fullyQualifiedNodePath = "/ns:$($tempPath.Replace($($using:NodeSeparatorCharacter), '/ns:'))"
-           Write-Verbose -Message "fullyQualifiedNodePath:$fullyQualifiedNodePath" -Verbose
-           $node = $XmlDocument.SelectSingleNode($fullyQualifiedNodePath, $xmlNsManager)
-           $node.SetAttribute($using:attributeName,$using:newValue)
-           $XmlDocument.Save($using:path)
-       }
-       TestScript = 
-       {
-           
-           $XmlDocument = [xml]$(get-content -Path $using:path -ErrorAction Stop)
-           $NamespaceURITemp = $using:NamespaceURI
-           if([string]::IsNullOrEmpty($NamespaceURITemp)) { $NamespaceURITemp = $XmlDocument.DocumentElement.NamespaceURI } 
-           $xmlNsManager = New-Object System.Xml.XmlNamespaceManager($XmlDocument.NameTable)
-           $xmlNsManager.AddNamespace("ns", $NamespaceURITemp)
-          
-           $tempPath = $using:NodePath
-           Write-Verbose -Message "tempPath:$tempPath" -Verbose
-           $fullyQualifiedNodePath = "/ns:$($tempPath.Replace($($using:NodeSeparatorCharacter), '/ns:'))"
-           Write-Verbose -Message "fullyQualifiedNodePath:$fullyQualifiedNodePath" -Verbose
-           $node = $XmlDocument.SelectSingleNode($fullyQualifiedNodePath, $xmlNsManager)
-           Write-Verbose -Message "node:$node" -Verbose
-           if($node -eq $null)
-           {
-                Write-Error "NodePath($using:NodePath) not found"
-                return $false
-
-           }
-           $cn = $($node.GetAttribute($using:attributeName))
-           $stateMatched = $cn -eq $using:newValue
-           return $stateMatched
-       }
-       DependsOn = @("[File]exists") 
-
-   }
+                TestScript = 
+                {
+                write-verbose -Message "Creating XMLDocument using Path:$($using:configPath)" -verbose
+                $XmlDocument = [xml]$(get-content -Path $($using:configPath) -ErrorAction Stop)
+                write-verbose -Message "Created XMLDocument:$XmlDocument" -verbose
+                $NamespaceURITemp = $using:NamespaceURI
+                if([string]::IsNullOrEmpty($NamespaceURITemp)) { $NamespaceURITemp = $XmlDocument.DocumentElement.NamespaceURI }
+                $xmlNsManager = New-Object System.Xml.XmlNamespaceManager($XmlDocument.NameTable)
+                $xmlNsManager.AddNamespace("ns", $NamespaceURITemp)
+                $tempPath = $using:nodePath
+                $fullyQualifiedNodePath = "/ns:$($tempPath.Replace('>', '/ns:'))"
+                $node = $XmlDocument.SelectSingleNode($fullyQualifiedNodePath, $xmlNsManager)
+                Write-Verbose -Message "node:$node" -Verbose
+                if($node -eq $null)
+                {
+                    Write-Error "NodePath($using:NodePath) not found"
+                    return $false
+                }
+                $cn = $($node.GetAttribute($attributeName))
+                $stateMatched = $cn -eq $newValue
+                return $stateMatched
+            }
+            
+                DependsOn = @("[File]FileCheck-$id")
+            }
+        }
    }
 }
-#Example Usage
-#[void]::(changeConfig -NodePath "nlog>targets>target" -path "C:\Users\caldanmaz\Downloads\NLog.config" -attributeName "Address" -newValue "Maliali" -verbose)
-#Start-DscConfiguration .\changeConfig -Wait -Force -ComputerName localhost -verbose
 
+changeConfig -Verbose
+Start-DscConfiguration .\changeConfig -Wait -Force -ComputerName localhost -verbose
